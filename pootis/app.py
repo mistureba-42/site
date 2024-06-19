ed292f6646cb6847206b99c5f7e697252fda70b6
guilherme <guilherme.napoleao@aluno.ifsp.edu.br>
2024-06-19 15:09:02 -0300

    cadastro de preco

--- pootis/app.py
+++ pootis/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+lista_produtos = [
+    ("nome", "coca-cola", "refrigerante doce")
+    ("doritos", "salgadinho farelento")
+    ("agua", "o mais saudavel")
+]
 
 @app.route("/")
 def home():
@@ -17,8 +23,25 @@ def produtos():
 
 @app.route("/produtos/<nome>")
 def produtos(nome):
-    for produto in lista_de_produtos:
+    for produto in lista_produtos:
         if produto["nome"] == nome:
             return render_template("produto.html", produto=produto)
 
     return "produto nao existe"
+
+@app.route("/produtos/cadastro")
+def cadastro_produto():
+    return render_template("cadastro-produto.html")
+
+@app.route("/produtos", methods=["POST"])
+def salvar_produto():
+    nome = request.form["nome"]
+    descricao = request.form["descricao"]
+    preco = request.form["preco"]
+    imagem = request.form["imagem"]
+
+    produto = {"nome" : nome, "descricao" : descricao, "preco" : preco, "imagem": imagem}
+    lista_produtos.append(produto)
+
+
+    return redirect(url_for("produtos"))
